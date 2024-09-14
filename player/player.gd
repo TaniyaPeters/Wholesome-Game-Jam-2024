225878c3a4df5d1ab95a1f5389cfa2c6279ac974
Alari Tork <alari.tork@gmail.com>
2024-09-14 17:38:49 -0400

    Updated player controller

--- player/player.gd
+++ player/player.gd
@@ -1,14 +1,24 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 600.0
 const JUMP_VELOCITY = -400.0
+
+const DEFAULT_GRAVITY_MULTIPLIER = 1.0
+const FALL_GRAVITY_MULTIPLIER = 1.5
+
+
+var gravity_multiplier := 1.0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if velocity.y > 0: # Falling
+			gravity_multiplier = FALL_GRAVITY_MULTIPLIER
+		else:
+			gravity_multiplier = DEFAULT_GRAVITY_MULTIPLIER
+		velocity += get_gravity() * gravity_multiplier * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
