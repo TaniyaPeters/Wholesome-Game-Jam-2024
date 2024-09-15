d922145f6281cdfd1af9d672d6143eb748cb85d4
Alari Tork <alari.tork@gmail.com>
2024-09-15 14:27:43 -0400

    Skullduggery with tapestry (It kinda works?)

--- player/camera_2d.gd
+++ player/camera_2d.gd
@@ -1,7 +1,8 @@
 extends Camera2D
 
 
-@export var player: Player2D
+@export var game_2d: Game2D
+@onready var player := game_2d.get_node("Player2D")
 
 
 const CAMERA_OFFSET := 100.0
@@ -11,12 +12,12 @@ var target_position: Vector2
 
 
 func _ready() -> void:
-	position = player.position
+	position = player.global_position
 	target_position = position
 
 
 func _physics_process(delta: float) -> void:
-	target_position.x = player.position.x
+	target_position.x = player.global_position.x
 	position = lerp(position, target_position, 0.2)
 
 	offset.x = lerp(offset.x, CAMERA_OFFSET * player.direction, 0.05)
---
+++ tapestry/full_tapestry_sub_viewport.gd
@@ -0,0 +1,5 @@
+extends SubViewport
+
+
+func _ready() -> void:
+	world_2d = %GameSubViewport.get_world_2d()
