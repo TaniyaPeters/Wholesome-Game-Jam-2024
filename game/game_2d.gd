905175020a5c29bab12c42334122bfc926ccb349
Alari Tork <alari.tork@gmail.com>
2024-09-15 00:43:37 -0400

    Updated pausing, started implementing collision shapes

--- game/game_2d.gd
+++ game/game_2d.gd
@@ -1,2 +1,16 @@
 class_name Game2D
 extends Node2D
+
+
+@onready var pausable_nodes := [
+	$Player
+]
+
+
+func pause():
+	for node:Node2D in pausable_nodes:
+		node.process_mode = Node.PROCESS_MODE_DISABLED
+
+func unpause():
+	for node:Node2D in pausable_nodes:
+		node.process_mode = Node.PROCESS_MODE_INHERIT
