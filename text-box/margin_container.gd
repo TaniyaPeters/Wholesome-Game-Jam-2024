9a1aef37e210206e33b4fb35f1439d100a4aeafd
TaniyaPeters <36174064+TaniyaPeters@users.noreply.github.com>
2024-09-15 14:47:04 -0400

    Got some dialog started

--- text-box/margin_container.gd
+++ text-box/margin_container.gd
@@ -1,7 +1,7 @@
 extends MarginContainer
 
-@onready var label = $Panel/MarginContainer/Panel/RichTextLabel
-@onready var timer = $LetterTimer
+@onready var label = $Panel2/RichTextLabel
+@onready var timer = $Timer
 
 const MAX_WIDTH =256
 
@@ -15,22 +15,17 @@ var punctuation_time = 0.2
 signal finished_displaying()
 
 func display_text(text_to_display:String):
-	print('hi')
 	text = text_to_display
 	label.text = text_to_display
-	print('there')
 	label.text = ""
-	
 	_display_letter()
 	
 func _display_letter():
-	label.text = text[letter_index]
+	label.text = text + text[letter_index]
 	letter_index += 1
-	print('here')
 	if letter_index >= text.length():
 		finished_displaying.emit()
 		return
-		
 	match text[letter_index]:
 		"!", "?":
 			timer.start(punctuation_time)
@@ -40,15 +35,6 @@ func _display_letter():
 			timer.start(letter_time)
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_letter_timer_timeout() -> void:
+func _on_timer_timeout() -> void:
 	_display_letter()
