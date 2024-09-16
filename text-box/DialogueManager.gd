9b8a979b241e92bd000820087e957eee83e93a76
TaniyaPeters <36174064+TaniyaPeters@users.noreply.github.com>
2024-09-15 20:47:08 -0400

    Iadded god script

--- text-box/DialogueManager.gd
+++ text-box/DialogueManager.gd
@@ -35,7 +35,7 @@ func _on_text_box_finished_displaying():
 
 func _unhandled_input(event):
 	if(
-		event.is_action_pressed("advance_dialog") &&
+		event.is_action_pressed("interact") &&
 		is_dialog_active && can_advance_line
 	):
 		
